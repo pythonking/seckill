6db690dd4effbb2449477fde843c6091170f80dd
wangquanwei <wqwking@126.com>
2018-04-25 17:34:48 +0800

    删除没有用到的引用

--- src/main/java/com/kaishengit/service/impl/ProductServiceImpl.java
+++ src/main/java/com/kaishengit/service/impl/ProductServiceImpl.java
@@ -22,7 +22,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -44,8 +43,6 @@ public class ProductServiceImpl implements ProductService {
     private String qiniuSK;
     @Value("${qiniu.buket}")
     private String qiniuBuket;
-    @Autowired
-    private JmsTemplate jmsTemplate;
     @Autowired
     private SchedulerFactoryBean schedulerFactoryBean;
 
