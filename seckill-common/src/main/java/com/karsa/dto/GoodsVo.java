e44898da35e6d69a5f408b29b42b3498420436ca
karsa <wqwking@126.com>
2021-07-08 15:31:04 +0800

    1.0.8：加入消息队列

--- seckill-common/src/main/java/com/karsa/dto/GoodsVo.java
+++ seckill-common/src/main/java/com/karsa/dto/GoodsVo.java
@@ -49,8 +49,20 @@ public class GoodsVo implements Serializable {
     /**
      * 只包含了部分 seckill_goods 表的信息
      */
+    /**
+     * 秒杀价格
+     */
     private BigDecimal seckillPrice;
+    /**
+     * 秒杀库存
+     */
     private Integer stockCount;
+    /**
+     * 开始日期
+     */
     private Date startDate;
+    /**
+     * 结束日期
+     */
     private Date endDate;
 }
