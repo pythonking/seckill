e347e7ec4310f1db6d4321b55570e31b78c3c4ea
karsa <wqwking@126.com>
2021-07-08 15:35:06 +0800

    1.0.8：加入消息队列

--- seckill-goods/src/main/java/com/karsa/mapper/GoodsMapper.java
+++ seckill-goods/src/main/java/com/karsa/mapper/GoodsMapper.java
@@ -40,7 +40,7 @@ public interface GoodsMapper extends BaseMapper<Goods> {
 
     /**
      * 减少 seckill_order 中的库存
-     * 增加库存判断 stock_count>0, 一次使得数据库不存在卖超问题,但是考虑到数据一致问题，还是要加上乐观锁判断
+     * 增加库存判断 stock_count > 0, 一次使得数据库不存在卖超问题,但是考虑到数据一致问题，还是要加上乐观锁判断
      *
      * @param goodsId
      */
