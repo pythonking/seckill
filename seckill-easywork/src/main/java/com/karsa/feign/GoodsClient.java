a04ef30f3820602659576f969da4554b069debcc
wangquanwei <wqwking@126.com>
2021-07-09 23:27:05 +0800

    1.0.8:集成easyexcel

--- seckill-easywork/src/main/java/com/karsa/feign/GoodsClient.java
+++ seckill-easywork/src/main/java/com/karsa/feign/GoodsClient.java
@@ -5,6 +5,7 @@ import com.karsa.dto.GoodsInfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -26,5 +27,5 @@ public interface GoodsClient {
      * @param infoList
      */
     @PostMapping(value = "/endpoint/batchInfoInsert")
-    Boolean batchInfoInsert(List<GoodsInfo> infoList);
+    Boolean batchInfoInsert(@RequestParam("infoList")List<GoodsInfo> infoList);
 }
--- seckill-goods/src/main/java/com/karsa/endpoint/GoodsEndpoint.java
+++ seckill-goods/src/main/java/com/karsa/endpoint/GoodsEndpoint.java
@@ -3,10 +3,7 @@ package com.karsa.endpoint;
 import com.karsa.dto.GoodsInfo;
 import com.karsa.service.IGoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -51,7 +48,7 @@ public class GoodsEndpoint {
      * @return
      */
     @PostMapping(value = "/batchInfoInsert")
-    public Boolean batchInfoInsert(List<GoodsInfo> infoList) {
+    public Boolean batchInfoInsert(@RequestParam("infoList") List<GoodsInfo> infoList) {
         return goodsService.batchInfoInsert(infoList);
     }
 
