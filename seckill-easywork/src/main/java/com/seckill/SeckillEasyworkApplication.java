9f0044dd8585957cf9c0c47d08bc62d448987ef2
wangquanwei <wqwking@126.com>
2021-07-09 11:10:16 +0800

    1.0.8:集成easyexcel

---
+++ seckill-easywork/src/main/java/com/seckill/SeckillEasyworkApplication.java
@@ -0,0 +1,13 @@
+package com.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillEasyworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillEasyworkApplication.class, args);
+    }
+
+}
