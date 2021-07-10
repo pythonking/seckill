f3982a4270f36d2857b0ceb0d9ef757e2bb8e1e4
wangquanwei <wqwking@126.com>
2021-07-10 20:12:11 +0800

    1.0.9:集成支付系统

---
+++ seckill-pay/src/main/java/com/karsa/controller/AliPayController.java
@@ -0,0 +1,15 @@
+package com.karsa.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 工具类
+ */
+@RestController
+@RequestMapping("/pay")
+public class AliPayController {
+
+
+}
+
