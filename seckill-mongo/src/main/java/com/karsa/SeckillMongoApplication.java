cb11236326a32066316582432bb2c216766e9f86
wqw <wqwking@126.com>
2021-07-20 12:24:08 +0800

    1.0.17:mongodb使用

---
+++ seckill-mongo/src/main/java/com/karsa/SeckillMongoApplication.java
@@ -0,0 +1,13 @@
+package com.karsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillMongoApplication.class, args);
+    }
+
+}
