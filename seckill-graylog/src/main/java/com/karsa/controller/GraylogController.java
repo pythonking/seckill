525cec162d5797b221cfe3b6b0257bade16ea093
wqw <wqwking@126.com>
2021-07-28 18:26:54 +0800

    1.0.21:配置graylog相关信息:docker-compose文件:调整为本地

--- seckill-graylog/src/main/java/com/karsa/controller/GraylogController.java
+++ seckill-graylog/src/main/java/com/karsa/controller/GraylogController.java
@@ -14,9 +14,9 @@ public class GraylogController {
     @GetMapping("/query")
     public Object queryLog() {
         for (int i = 0; i < 6; i++) {
-            log.info("info:{}","info 日志打印" + i);
-            log.warn("warn:{}","warn 日志打印" + i);
-            log.error("error:{}","error 日志打印" + i);
+            log.info("info:{}","info 昨天打印" + i);
+            log.warn("warn:{}","warn 明天打印" + i);
+            log.error("error:{}","error 后天打印" + i);
         }
         return "success";
     }
