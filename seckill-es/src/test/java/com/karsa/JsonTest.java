075543ca1ea52eb9bfa735f0639c1bc33933d6e0
wqw <wqwking@126.com>
2021-07-30 13:25:59 +0800

    1.0.23:设置索引mapping

--- seckill-es/src/test/java/com/karsa/JsonTest.java
+++ seckill-es/src/test/java/com/karsa/JsonTest.java
@@ -9,6 +9,9 @@ import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.PutMappingRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -45,5 +48,63 @@ public class JsonTest {
         BulkResponse bulk = restHighLevelClient.bulk(bulkRequest, RequestOptions.DEFAULT);
         //是否失败 false 就是成功
         log.info("获取结果 {} ", bulk.hasFailures());
+    }
+
+    /**
+     * 特殊的批量插入数据
+     *
+     * @throws IOException
+     */
+    @Test
+    public void createIndex() throws IOException{
+        //1.创建索引请求
+        CreateIndexRequest request = new CreateIndexRequest("somehi_index") ;
+        request.mapping("{\n" +
+                "  \"properties\": {\n" +
+                "    \"author\": {\n" +
+                "      \"type\": \"text\",\n" +
+                "      \"fields\": {\n" +
+                "        \"field\": {\n" +
+                "          \"type\": \"keyword\"\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"analyzer\": \"ik_max_word\"\n" +
+                "    },\n" +
+                "    \"contents\": {\n" +
+                "      \"type\": \"text\",\n" +
+                "      \"fields\": {\n" +
+                "        \"field\": {\n" +
+                "          \"type\": \"keyword\"\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"analyzer\": \"ik_max_word\",\n" +
+                "      \"fielddata\": true\n" +
+                "    },\n" +
+                "    \"timestamp\": {\n" +
+                "      \"type\": \"date\"\n" +
+                "    },\n" +
+                "    \"title\": {\n" +
+                "      \"type\": \"text\",\n" +
+                "      \"fields\": {\n" +
+                "        \"field\": {\n" +
+                "          \"type\": \"keyword\"\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"analyzer\": \"ik_max_word\"\n" +
+                "    },\n" +
+                "    \"type\": {\n" +
+                "      \"type\": \"text\",\n" +
+                "      \"fields\": {\n" +
+                "        \"field\": {\n" +
+                "          \"type\": \"keyword\"\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"analyzer\": \"ik_max_word\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}",XContentType.JSON);
+        //2.执行请求
+        CreateIndexResponse response = restHighLevelClient.indices().create(request, RequestOptions.DEFAULT);
+        log.info("获取创建索引 {}", response.index());
     }
 }
